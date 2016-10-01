140f55b8b292ceef6d0b641bd753382774f1a404
suryasr007 <94suryateja@gmail.com>
2016-10-01 10:53:37 +0530

    references added using this keyword

--- toDo.js
+++ toDo.js
@@ -1,22 +1,22 @@
 var toDoList={
-    todos:["item1","item2","item3","item4"];
+    todos:["item1","item2","item3","item4"],
     
     displayToDos: function(){
         console.log("my ToDo list", this.todos);
-    }
+    },
 
     addToDos: function(todo){
         this.todos.pust(todo);
         this.displayToDos();
-    }
+    },
 
     changeToDos:function(position,newValue){
         this.todos[position]=newValue;
         this.displayToDos();
-    }
+    },
     
     deleteToDos:function(position){
         this.todos.splice(position, 1);
         this.displayToDos();
-    }
+    },
 }
