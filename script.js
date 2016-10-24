017179872e4738bd75bf60f21a51b697e592231f
suryasr007 <94suryateja@gmail.com>
2016-10-24 21:37:12 +0530

    Manupulating DOM And escaping from console

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
     var toDoList={
         todos:[],
 
-        displayToDos: function(){
-            //debugger; it is used to debug the javascript code in the browser.
-            if(this.todos.length === 0)
-                console.log("Your To-do list is empty");
-            else{
-                console.log("my ToDo list:");
-                for(var i=0;i<this.todos.length;i++){
-                    if(this.todos[i].completed=== true){ // verying whether EACH todo is completed or not
-                        console.log('(x) ',this.todos[i].todoText);
-                }
-                    else
-                        console.log('() ', this.todos[i].todoText);
-                    }
-                }
-        },
+//        displayToDos: function(){
+//            //debugger; it is used to debug the javascript code in the browser.
+//            if(this.todos.length === 0)
+//                console.log("Your To-do list is empty");
+//            else{
+//                console.log("my ToDo list:");
+//                for(var i=0;i<this.todos.length;i++){
+//                    if(this.todos[i].completed=== true){ // verying whether EACH todo is completed or not
+//                        console.log('(x) ',this.todos[i].todoText);
+//                }
+//                    else
+//                        console.log('() ', this.todos[i].todoText);
+//                    }
+//                }
+//        },
 
         addToDos: function(todoText){  //Pusing objects instead of text to the array
             //debugger;
@@ -23,24 +23,24 @@
                 todoText:todoText,     // Yes, we can declare 
                 completed: false
             });
-            this.displayToDos();
+//            this.displayToDos();
         },
 
         changeToDos:function(position,newtodoText){
             //this.todos[position]=newValue;
             this.todos[position].todoText=newtodoText;
-            this.displayToDos();
+//            this.displayToDos();
         },
 
         deleteToDos:function(position){
             this.todos.splice(position, 1);
-            this.displayToDos();
+//            this.displayToDos();
         },
 
         toggleToDos:function(position){
             var todo = this.todos[position];
             todo.completed=!todo.completed;
-            this.displayToDos();
+//            this.displayToDos();
         },
         
         toggleAll:function(){
@@ -63,7 +63,7 @@
                     this.todos[i].completed=true;
             }
             
-            this.displayToDos();
+//            this.displayToDos();
         
         }
         
@@ -72,35 +72,82 @@
    
 var handlers={
     
-    displayToDosButton : function(){
-        toDoList.displayToDos();
-    },
-    
+//    displayToDosButton : function(){
+//        toDoList.displayToDos();
+//    },
+
     toggleAllButton : function(){
         toDoList.toggleAll();
+        view.displayToDos();
     },
     
     addToDoButton :function(){
         var addToDoValue = document.getElementById('valueEnteredInTextBox');
         toDoList.addToDos(addToDoValue.value);
         addToDoValue.value = '';
+        view.displayToDos();
     },
     
     toDoChangeButton : function(){
         var changeToDoPosition = document.getElementById('toDoChangePosition');
         var changeToDoValue = document.getElementById('toDoChangeText');
         toDoList.changeToDos(toDoChangePosition.valueAsNumber,toDoChangeText.value);
+        changeToDoValue.value = '';
+        view.displayToDos();
     },
     
     toDoDeleteButton : function(){
         var deleteToDo =document.getElementById('deletePosition');
         toDoList.deleteToDos(deleteToDo.valueAsNumber);
-        deleteToDo ='';
+        deleteToDo.value ='';
+        view.displayToDos();
     },
     toDoToggelButton : function(){
         var toogleToDo = document.getElementById('togglePosition');
         toDoList.toggleToDos(toogleToDo.valueAsNumber);
-        toogleToDo='';
+        toogleToDo.value='';
+        view.displayToDos();
     }
 };
+
+var view ={  // to display the todos a li elements.
+    displayToDos : function(){
+        var toDoUl = document.querySelector('ul');  // Selecting the required query in DOM
+        toDoUl.innerHTML = '';
+        for(var i=0; i<toDoList.todos.length; i++){
+             var toDoLi = document.createElement('li'); // creating the li element;
+             var toDo = toDoList.todos[i];
+             var toDosWithCompletion = ''; 
+            
+             if(toDo.completed === true){  // displaying toDo along with its completion status.
+                 toDosWithCompletion ='(x) ' + toDo.todoText;
+             }
+             else{
+                toDosWithCompletion ='( ) ' + toDo.todoText;
+             }
+            
+             toDoLi.textContent=toDosWithCompletion;
+             toDoUl.appendChild(toDoLi);
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
